4e81f5ac638bd05da8f00c73e12eb3066d64402d
Steven Silvestri <steven.m.silvestri@gmail.com>
2019-06-13 10:40:42 -0600

    refactor(redis): Provide clients a way to customize annotations
    Attempts to try and mark some other address features (doesn't seem to be working yet)

--- src/redis/zipkin-redis-client.ts
+++ src/redis/zipkin-redis-client.ts
@@ -1,22 +1,21 @@
 import { ClientRedis } from '@nestjs/microservices';
-import { Logger } from '@nestjs/common';
+import os from 'os';
 import { Tracer, InetAddress, TraceId, Annotation } from 'zipkin';
 import { tap } from 'rxjs/operators';
-import { ZipkinRedisOptions } from './zipkin-redis.interfaces';
-import { createTracer, getTraceId } from '../common';
+import { ZipkinRedisOptions, ServiceAddress } from './zipkin-redis.interfaces';
+import { createTracer, getTraceId, recordObjectAsBinary } from '../common';
 import { TraceablePayload } from '../zipkin.interfaces';
 
 export type TracerMetaFn = (tracer: Tracer) => void;
-const noop = () => {};
+export type AdditionalAnnotations = {
+  [key: string]: string | boolean | number;
+};
 
 export class ZipkinRedisClient extends ClientRedis {
   private readonly tracer: Tracer;
   private readonly serviceName: string;
-  private readonly serverAddress: {
-    serviceName: string;
-    host: InetAddress;
-    port: number;
-  };
+  private readonly serverAddress: ServiceAddress;
+  private readonly clientAddress: ServiceAddress;
 
   public constructor(
     { serviceName, host, port }: ZipkinRedisOptions,
@@ -31,39 +30,35 @@ export class ZipkinRedisClient extends ClientRedis {
       host: new InetAddress(host),
       port,
     };
+
+    this.clientAddress = {
+      serviceName,
+      host: new InetAddress(os.hostname()),
+      port,
+    };
   }
 
-  public send(pattern: string, data: any) {
-    const traceId = this.recordTrace(
-      pattern,
-      typeof data === 'object' ? data : { payload: data, traceId: undefined }
-    );
-
-    this.tracer.setId(traceId);
+  public send(pattern: string, data: any, annotations: AdditionalAnnotations = {}) {
+    const traceId = this.recordSendTrace(pattern, data, annotations);
 
     return super
       .send(pattern, { ...data, traceId: traceId.traceId })
       .pipe(tap(this.recordAckTrace(traceId)));
   }
 
-  public emit(pattern: any, data: any, tracerCb: TracerMetaFn = noop) {
-    const trace = this.recordSendTrace(pattern, data);
-
-    try {
-      tracerCb(this.tracer);
-    } catch (e) {
-      this.logger.warn(e.stack);
-    }
+  public emit(pattern: any, data: any, annotations: AdditionalAnnotations = {}) {
+    const trace = this.recordSendTrace(pattern, data, annotations);
 
     return super
       .emit(pattern, { ...data, traceId: trace.traceId })
       .pipe(tap(this.recordAckTrace(trace)));
   }
 
-  private recordSendTrace(pattern: any, data: any) {
+  private recordSendTrace(pattern: any, data: any, annotations: AdditionalAnnotations) {
     const traceId = this.recordTrace(
       pattern,
-      typeof data === 'object' ? data : { payload: data, traceId: undefined }
+      typeof data === 'object' ? data : { payload: data, traceId: undefined },
+      annotations
     );
 
     this.tracer.setId(traceId);
@@ -74,20 +69,27 @@ export class ZipkinRedisClient extends ClientRedis {
     return () => {
       this.tracer.letId(trace, () => {
         this.tracer.recordAnnotation(new Annotation.ClientRecv());
+        this.tracer.recordAnnotation(new Annotation.ServerSend());
       });
     };
   }
 
-  private recordTrace(pattern: any, data: TraceablePayload) {
+  private recordTrace(pattern: any, data: TraceablePayload, annotations: AdditionalAnnotations) {
     const traceId = getTraceId(this.tracer, data);
 
-    this.logger.debug(`Recording trace with: ${traceId}`);
+    this.logger.debug(
+      `Recording trace with: ${traceId} and annotations: ${JSON.stringify(annotations)}`
+    );
 
     this.tracer.letId(traceId, () => {
-      this.tracer.recordRpc(JSON.stringify(pattern));
-      this.tracer.recordAnnotation(new Annotation.ServiceName(this.serviceName));
-      this.tracer.recordAnnotation(new Annotation.ServerAddr(this.serverAddress));
       this.tracer.recordAnnotation(new Annotation.ClientSend());
+      this.tracer.recordAnnotation(new Annotation.ServerAddr(this.serverAddress));
+      this.tracer.recordAnnotation(new Annotation.ServiceName(this.serviceName));
+      this.tracer.recordClientAddr(new InetAddress(os.hostname()));
+      this.tracer.recordRpc(JSON.stringify(pattern));
+      recordObjectAsBinary(this.tracer, annotations);
+
+      this.tracer.recordAnnotation(new Annotation.ClientAddr(this.clientAddress));
     });
 
     return traceId;
